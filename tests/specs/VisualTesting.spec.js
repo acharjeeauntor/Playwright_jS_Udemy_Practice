9d25d60ee8971c867f102a4cfcd8abbbc82acf6b
BroTecs-000249 <brotecs-000249@BroTecss-Mac-mini-3.local>
2022-05-31 17:46:42 +0600

    Added

--- tests/specs/VisualTesting.spec.js
+++ tests/specs/VisualTesting.spec.js
@@ -7,6 +7,15 @@ test("Visual Testing : Screenshot", async ({ page }) => {
     await page.locator("#hide-textbox").click()
     await expect(page.locator("#displayed-text")).toBeHidden()
     await page.screenshot({path:"SS2.png"})
-   
 
+})
+
+test.only("Visual Testing : Comparision-pass",async({page})=>{
+    await page.goto("https://google.com")
+    expect(await page.screenshot()).toMatchSnapshot("loading.png")
+})
+
+test("Visual Testing : Comparision-Fail",async({page})=>{
+    await page.goto("https://www.rediff.com/")
+    expect(await page.screenshot()).toMatchSnapshot("mainPage.png")
 })
